1c6509721ce3614531cfa76a5f4587484d62bc85
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-03-16 14:57:11 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const wildguess = document.getElementById("text");
+    wildguess.textContent = "This is really cool!";
+});
